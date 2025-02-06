4c5d9ae47a010133ca139e140e261088e9957f39
Nick <jaman228822@mail.ru>
2025-02-06 18:49:16 +0500

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
 import './styles/App.scss';
 import { FC } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import ChoozePage from './pages/ChoozePage';
 import CameraPage from './pages/CameraPage';
 
 const App: FC = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<CameraPage />} />
-            {/* <Route path="/camera" element={<CameraPage />} /> */}
-        </Routes>
-    );
+    return <CameraPage />;
 };
 
 export default App;
